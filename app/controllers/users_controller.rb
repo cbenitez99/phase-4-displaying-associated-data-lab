817cf3d9a4862ad84cf1c972571c46ec03a8a340
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-01-25 15:54:43 -0600

    commit tree 49e44ee55f9dd606b62e3b2c75b1f60634adb59e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by_id(params[:id])
+        if user
+            render json: user, include: :items
+        end
+    end
 end
